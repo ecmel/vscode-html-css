98e577f685a648333180892aaf5ed1c41a0ebe4c
Ecmel Ercan <Ecmel Ercan>
2016-07-16 21:54:15 +0300

    Add initial class completion support

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,6 @@ import * as lst from 'vscode-languageserver-types';
 import * as css from 'vscode-css-languageservice';
 
 let service = css.getCSSLanguageService();
-let styleRegEx = /<style>([^<]*$)/;
-let styleInlineRegEx = /style=["|']([^"^']*$)/;
 
 class Snippet {
 
@@ -14,29 +12,10 @@ class Snippet {
     private _stylesheet: css.Stylesheet;
     private _position: lst.Position;
 
-    constructor(document: vsc.TextDocument, position: vsc.Position) {
-
-        let start = new vsc.Position(0, 0);
-        let range = new vsc.Range(start, position);
-        let text = document.getText(range);
-
-        let tag = styleInlineRegEx.exec(text);
-        if (tag) {
-            this.init('.c {\n' + tag[1], position.character);
-            return;
-        }
-
-        tag = styleRegEx.exec(text);
-        if (tag) {
-            this.init(tag[1], position.character);
-            return;
-        }
-    }
-
-    private init(content: string, character: number): void {
+    constructor(content: string, character?: number) {
         this._document = lst.TextDocument.create('', 'css', 1, content);
         this._stylesheet = service.parseStylesheet(this._document);
-        this._position = new vsc.Position(this._document.lineCount - 1, character);
+        this._position = new vsc.Position(this._document.lineCount - 1, character ? character : 0);
     }
 
     public get document(): lst.TextDocument {
@@ -52,7 +31,9 @@ class Snippet {
     }
 }
 
-class LanguageServer implements vsc.CompletionItemProvider, vsc.HoverProvider {
+class StyleServer implements vsc.CompletionItemProvider, vsc.HoverProvider {
+
+    private regex = [/style=["|']([^"^']*$)/, /<style>([^<]*$)/];
 
     private convertCompletionList(list: lst.CompletionList): vsc.CompletionList {
         let ci: vsc.CompletionItem[] = [];
@@ -68,10 +49,28 @@ class LanguageServer implements vsc.CompletionItemProvider, vsc.HoverProvider {
         return new vsc.CompletionList(ci, list.isIncomplete);
     }
 
-    provideCompletionItems(document: vsc.TextDocument, position: vsc.Position, token: vsc.CancellationToken): vsc.CompletionList {
-        let snippet = new Snippet(document, position);
+    private createSnippet(document: vsc.TextDocument, position: vsc.Position): Snippet {
+        let start = new vsc.Position(0, 0);
+        let range = new vsc.Range(start, position);
+        let text = document.getText(range);
 
-        if (snippet.document) {
+        let tag = this.regex[0].exec(text);
+        if (tag) {
+            return new Snippet('.c {\n' + tag[1], position.character);
+        }
+
+        tag = this.regex[1].exec(text);
+        if (tag) {
+            return new Snippet(tag[1], position.character);
+        }
+
+        return null;
+    }
+
+    provideCompletionItems(document: vsc.TextDocument, position: vsc.Position, token: vsc.CancellationToken): vsc.CompletionList {
+        let snippet = this.createSnippet(document, position);
+
+        if (snippet) {
             let result = service.doComplete(snippet.document, snippet.position, snippet.stylesheet);
             return this.convertCompletionList(result);
         }
@@ -83,9 +82,9 @@ class LanguageServer implements vsc.CompletionItemProvider, vsc.HoverProvider {
     }
 
     provideHover(document: vsc.TextDocument, position: vsc.Position, token: vsc.CancellationToken): vsc.Hover {
-        let snippet = new Snippet(document, position);
+        let snippet = this.createSnippet(document, position);
 
-        if (snippet.document) {
+        if (snippet) {
             let result = service.doHover(snippet.document, snippet.position, snippet.stylesheet);
             return new vsc.Hover(result.contents);
         }
@@ -93,12 +92,54 @@ class LanguageServer implements vsc.CompletionItemProvider, vsc.HoverProvider {
     }
 }
 
+class ClassServer implements vsc.CompletionItemProvider {
+
+    private regex = [/class=["|']([^"^']*$)/, /<style>([\s\S]*)<\/style>/];
+
+    provideCompletionItems(document: vsc.TextDocument, position: vsc.Position, token: vsc.CancellationToken): vsc.CompletionList {
+        let start = new vsc.Position(0, 0);
+        let range = new vsc.Range(start, position);
+        let text = document.getText(range);
+
+        let tag = this.regex[0].exec(text);
+        if (tag) {
+            let style = this.regex[1].exec(document.getText());
+            if (style) {
+                let snippet = new Snippet(style[1]);
+                let symbols = service.findDocumentSymbols(snippet.document, snippet.stylesheet);
+                let ci: vsc.CompletionItem[] = [];
+                for (let i = 0; i < symbols.length; i++) {
+                    if (symbols[i].kind !== 5) {
+                        continue;
+                    }
+                    let c = symbols[i].name.split(/\s/);
+                    for (let j = 0; j < c.length; j++) {
+                        if (c[j].startsWith('.')) {
+                            ci.push(new vsc.CompletionItem(c[j].substr(1)));
+                        }
+                    }
+                }
+                return new vsc.CompletionList(ci);
+            }
+        }
+        return null;
+    }
+
+    resolveCompletionItem(item: vsc.CompletionItem, token: vsc.CancellationToken): vsc.CompletionItem {
+        return null;
+    }
+}
+
 export function activate(context: vsc.ExtensionContext) {
 
-    let server = new LanguageServer();
+    let styleServer = new StyleServer();
 
-    context.subscriptions.push(vsc.languages.registerCompletionItemProvider('html', server));
-    context.subscriptions.push(vsc.languages.registerHoverProvider('html', server));
+    context.subscriptions.push(vsc.languages.registerCompletionItemProvider('html', styleServer));
+    context.subscriptions.push(vsc.languages.registerHoverProvider('html', styleServer));
+
+    let classServer = new ClassServer();
+
+    context.subscriptions.push(vsc.languages.registerCompletionItemProvider('html', classServer));
 }
 
 export function deactivate() {
