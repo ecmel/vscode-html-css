8c54162cb8b759c69d6906b63ce1eb56fa63869e
Ecmel Ercan <Ecmel Ercan>
2016-08-21 13:16:25 +0300

    Begin testing

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -14,9 +14,11 @@ import * as myExtension from '../src/extension';
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+  // Defines a Mocha unit test
+  test("html", () => {
+    let uri = vscode.Uri.parse('file:///' + vscode.workspace.rootPath + '/test.html');
+    vscode.workspace.openTextDocument(uri).then(function (doc: vscode.TextDocument) {      
+      assert.equal('test.html', doc.fileName);
     });
+  });
 });
