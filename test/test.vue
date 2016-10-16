ee3fc5e48f1ca69d8cbda022726b0ad641ba7592
Ecmel Ercan <Ecmel Ercan>
2016-10-16 22:35:14 +0300

    Add vue test

---
+++ test/test.vue
@@ -0,0 +1,25 @@
+<style>
+	.internal {
+		color: red;
+	}
+	
+	.internal-test1 .internal-test2 {
+		color: red;
+	}
+	
+	#internal_id {
+		color: red;
+	}
+	
+	#internal_id-test1 #internal_id-test2 {
+		color: red;
+	}
+</style>
+
+<template>
+	<div id="internal_id" class="internal"></div>
+</template>
+
+<script>
+  console.log("Hello World!");
+</script>
