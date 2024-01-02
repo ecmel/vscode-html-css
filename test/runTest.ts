61ca69800a9368700e83445b6200139dda57d41b
ecmel <ecmel.ercan@gmail.com>
2024-01-02 07:13:55 +0300

    fixed test

--- test/runTest.ts
+++ test/runTest.ts
@@ -3,15 +3,13 @@
  * Licensed under the MIT License
  */
 
-import * as path from "path";
-
+import path from "path";
 import { runTests } from "@vscode/test-electron";
 
 async function main() {
   try {
     const extensionDevelopmentPath = path.resolve(__dirname, "../../");
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
-
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
   } catch (err) {
     console.error("Failed to run tests");
--- test/suite/settings.test.ts
+++ test/suite/settings.test.ts
@@ -4,23 +4,35 @@
  */
 
 import assert from "assert";
-import { describe, it } from "mocha";
-import * as settings from "../../src/settings";
+import sinon from "sinon";
+import { afterEach, describe, it } from "mocha";
 import { workspace } from "vscode";
+import * as settings from "../../src/settings";
 
 describe("settings", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
   it("should return enabled languages", () => {
+    sinon
+      .stub(workspace, "getConfiguration")
+      .value(() => ({ get: () => ["html"] }));
     const enabledLanguages = settings.getEnabledLanguages();
     assert.strictEqual(enabledLanguages.length, 1);
     assert.strictEqual(enabledLanguages[0], "html");
   });
 
   it("should return style sheets", async () => {
+    sinon
+      .stub(workspace, "getConfiguration")
+      .value(() => ({ get: () => ["some"] }));
     const document = await workspace.openTextDocument({
       language: "html",
       content: "",
     });
     const styleSheets = settings.getStyleSheets(document.uri);
-    assert.strictEqual(styleSheets.length, 0);
+    assert.strictEqual(styleSheets.length, 1);
+    assert.strictEqual(styleSheets[0], "some");
   });
 });
